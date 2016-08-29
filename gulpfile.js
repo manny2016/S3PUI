02ef8842b4ffc33a3b316d3ed11e80257fa03d1c
Gary Liu (Wicresoft) <v-guiliu@microsoft.com>
2016-08-29 21:16:24 +0800

    gulp add

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+//var babel = require('babelify');
+
+gulp.task('browserify', function () {
+    // Grabs the app.js file
+    return browserify('src/js/app.js', { debug: true })
+        //.transform(babel, { presets: ['es2015'] })
+        // bundles it and creates a file called main.js
+        .bundle()
+        .pipe(source('main.js'))
+        // saves it the public/js/ directory
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('js/src/**/*.js', ['browserify'])
+});
+
+gulp.task('default', ['watch']);
