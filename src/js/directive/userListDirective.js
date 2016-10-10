b9d50fafd99ecf3228957681d1793c0514407872
Gary Liu (Wicresoft) <v-guiliu@microsoft.com>
2016-10-10 17:22:08 +0800

    fix #40

--- src/js/directive/userListDirective.js
+++ src/js/directive/userListDirective.js
@@ -19,7 +19,7 @@ module.exports = function ($rootScope) {
             scope.pnscope = scope.pnscope || 'all';
             scope.getAvatorUrl = function(user){
                 return user.attachedobject.profile_image|| user.attachedobject.profile_image_url ||'public/images/avator.png'
-            } 
+            }
             scope.onClick = function (user) {
                 if (a.noPop === undefined) {
                     var param = {
