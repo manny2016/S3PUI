70c281032112f3daa54f2343155738c85f166b64
Gary Liu (Wicresoft) <v-guiliu@microsoft.com>
2016-11-30 14:49:54 +0800

    merge local with v2

--- src/js/controller/adminCtrl.js
+++ src/js/controller/adminCtrl.js
@@ -1,11 +1,16 @@
 module.exports = function ($scope, $location, $timeout) {
     // console.log("this is admin");  
+
+    $scope.selectPlatform = function (platform) {
+        $scope.selectedPlatform = platform;
+        $scope.$broadcast('admin-select-platform',platform)
+    }
     $scope.getPlatforms = function () {
         //simulate api calling
         $timeout(function () {
-                $scope.platforms = ['twitter','so','sf','su','msdn','tn']
-            }, 500)
-    }();  
+            $scope.platforms = ['twitter', 'so', 'sf', 'su', 'msdn', 'tn']
+        }, 500)
+    } ();
     // $('.admin.cards .card').dimmer({
     //     on: 'hover'
     // });
