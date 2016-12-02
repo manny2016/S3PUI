c0bc0172a811903ec1a3c487f8718a956fc90bbb
Gary Liu (Wicresoft) <v-guiliu@microsoft.com>
2016-12-02 18:46:44 +0800

    fix issue

---
+++ Index.aspx.cs
@@ -0,0 +1,61 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+
+
+namespace S3PWebUI
+{
+    public partial class Index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.SignedPH.Visible = false;
+            this.SignInPH.Visible = false;
+            if (!IsPostBack)
+            {
+                if (Request.IsAuthenticated)
+                {
+                    this.lblUserName.Text = HttpContext.Current.User.Identity.Name;
+                    this.SignedPH.Visible = true; ;
+                }
+                else
+                {
+                    this.SignInPH.Visible = true;
+                }
+            }
+        }
+
+        private bool CheckLogin()
+        {
+            return true;
+        }
+
+        protected void signBtn_Click(object sender, EventArgs e)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "index.aspx"}, OpenIdConnectAuthenticationDefaults.AuthenticationType
+                    );
+            }
+        }
+
+        protected void signoutBtn_Click(object sender, EventArgs e)
+        {
+            HttpContext.Current.GetOwinContext().Authentication.SignOut(
+                   OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType
+                   );
+        }
+    }
+}
---
+++ default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace S3PWebUI
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public string UserAlias;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated)
+            {
+                this.UserAlias = HttpContext.Current.User.Identity.Name.Replace("live.com#", string.Empty);
+            }
+        }
+        protected void signoutBtn_Click(object sender, EventArgs e)
+        {
+            HttpContext.Current.GetOwinContext().Authentication.SignOut(
+                   OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType
+                   );
+        }
+    }
+}
