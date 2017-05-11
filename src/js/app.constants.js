4f7a3d1344b5f495201066b1913af6ec050b93e2
Wheeler Tse <darkwanderer.1985@gmail.com>
2017-05-11 13:09:17 +0800

    change to using slicing server

--- src/js/app.constants.js
+++ src/js/app.constants.js
@@ -20,7 +20,7 @@ app.constant('CONST', {
         'SupportExperienceIssuesDetected': 'Support experience issues'
     },
     SERVICE_INFO: {
-        ENDPOINT: 'https://shpv2-orchard-01.azurewebsites.net/WebServices/S3PDataService/',
+        ENDPOINT: 'https://shpv2-uat-slicing.azurewebsites.net/WebServices/S3PDataService/',
         LOCAL_TEST_DATA: '/data/',
         TWITTER_SERVER_STATUS:'https://shpv2-uat-slicing.azurewebsites.net/WebServices/S3PDataService/GetJobStatus',
         OTHERS_SERVER_STATUS:'https://shpv2-orchard-01.azurewebsites.net/WebServices/S3PDataService/GetJobStatus'
