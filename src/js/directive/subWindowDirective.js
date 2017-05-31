889c7d30972f572acd23f3d9a5acf09687723b3d
Wheeler Tse <darkwanderer.1985@gmail.com>
2017-05-31 18:01:18 +0800

    fix issue of country "United States of America"

--- src/js/directive/subWindowDirective.js
+++ src/js/directive/subWindowDirective.js
@@ -17,8 +17,8 @@ module.exports = /*@ngInject*/ function ($rootScope, $window, $compile, $filter,
             //console.log($(e).find('.hourly-charts'))
             scope.myChart = echarts.init($(e).find('.hourly-charts').get(0));
             scope.getData = function (params) {
-                var country = params.param.country;
-                if (country === "United States of America") { country = "United States"; }
+                var country = params.param.country.toLowerCase();
+                if (country === "united states of america") { country = "united states"; }
                 $window.threadOption = {
                     function: params.fn,
                     platform: params.param.platform.toLowerCase(),
