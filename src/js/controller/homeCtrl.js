8c84770f15d56c3d6032aa790595d319eb1d721b
Wheeler Tse <darkwanderer.1985@gmail.com>
2017-08-22 13:02:32 +0800

    #431 change ui follow comment

--- src/js/controller/homeCtrl.js
+++ src/js/controller/homeCtrl.js
@@ -1,18 +1,23 @@
 module.exports = function ($scope, $rootScope, $window, $timeout, $http, $q, $sce, $compile, $document, $websocket, CONST, utilitySrv) {
     //// the time is UTC date time value in locale timezone in here.
     //// example: CST (GMT+8), now is 2017-01-01 09:00:00 in locale, it should be "2017-01-01 01:00:00 GMT+0800 (China Standard Time)"
-    var today = parseInt((new Date()).valueOf() / 3600000 / 24) * 24 * 3600000 + (new Date()).getTimezoneOffset() * 60000;
+    var timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
+    var today = parseInt((new Date()).valueOf() / 3600000 / 24) * 24 * 3600000 + timezoneOffset;
     var start = new Date(today - 3600000 * 24 * 7), end = new Date(today - 3600000 * 24), outset = new Date(today - 3600000 * 24 * 30);
     function datetimeChanged() {
         var dtStart = dtpStart.value();
         var dtEnd = dtpEnd.value();
 
         if (dtStart) {
-            dtpEnd.min(new Date(dtStart));
+            dtpEnd.min(dtStart);
         }
         if (dtEnd) {
-            dtpStart.max(new Date(dtEnd));
+            dtpStart.max(dtEnd);
         }
+
+        $scope.startDateLocalsString = (new Date(dtStart.valueOf() - timezoneOffset)).toLocaleString();
+        $scope.endDateLocalsString = (new Date(dtEnd.valueOf() + 86400000 - timezoneOffset)).toLocaleString();
+        $scope.$apply();
         $scope.startGetData($rootScope.global.topic);
     }
     $("#ddlQuicklyTest").kendoDropDownList({
@@ -26,7 +31,7 @@ module.exports = function ($scope, $rootScope, $window, $timeout, $http, $q, $sc
         }
     });
     var dtpStart = $('#DateTimePickerStart').kendoDatePicker({
-        value: outset,//start,
+        value: start,
         format: localeDateFormatString,
         change: datetimeChanged,
         min: new Date(2016, 7, 1),//outset,
@@ -44,11 +49,11 @@ module.exports = function ($scope, $rootScope, $window, $timeout, $http, $q, $sc
 
     $scope.query = {
         granularity: 3, //// daily
-        start: outset,
-        end: end,
-        StartDate: (new Date(outset)).toLocaleString(),
-        EndDate: (new Date(end)).toLocaleString(),
+        start: start,
+        end: end
     };
+    $scope.startDateLocalsString = (new Date(start - timezoneOffset)).toLocaleString();
+    $scope.endDateLocalsString = (new Date(today - timezoneOffset)).toLocaleString();
     // var totalrequests = 28+12;
     var sections = 8,
         totalrequests = 0;
@@ -126,12 +131,10 @@ module.exports = function ($scope, $rootScope, $window, $timeout, $http, $q, $sc
             $('#progress').show();
 
             var dtStart = dtpStart.value(), dtEnd = dtpEnd.value();
-            dtStart = dtStart.valueOf() - dtStart.getTimezoneOffset() * 60000;
-            dtEnd = dtEnd.valueOf() - dtEnd.getTimezoneOffset() * 60000;
+            dtStart = dtStart.valueOf() - timezoneOffset;
+            dtEnd = dtEnd.valueOf() - timezoneOffset;
             $scope.query.start = dtStart;
             $scope.query.end = dtEnd;
-            $scope.StartDate = (new Date(dtStart)).toLocaleString();
-            $scope.EndDate = (new Date(dtEnd)).toLocaleString();
             var offsetDays = (dtEnd - dtStart) / 3600 / 24 / 1000;
             $('#volumes > div.content > div:nth-child(2)')
                 .attr('class', 'ui ' + (offsetDays <= 30 ? 'three' : (offsetDays <= 60 ? 'two' : 'one')) + ' column grid');
